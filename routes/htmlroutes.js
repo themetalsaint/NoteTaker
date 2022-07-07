e9db6fe06bb7c5ea93bbf33c0750241ab6441c61
Rachael <themetalsaint@gmail.com>
2022-07-07 13:27:07 -0400

    Working on Delete code

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -12,4 +12,17 @@ module.exports= (db)=>{
         res.sendFile(path.join(__dirname, "../public/index.html"))
     })
 
+    //Working on Delete
+//     app.delete('/api/notes/:id', (req, res) => {
+//     const chosenId = req.params.id
+//     let notes = require('../db/db.json');
+//     for (let i = 0; i < notes.length; i++) {
+//       if (notes[i].id === chosenId) {
+//         notes.splice(i, 1)
+//       }
+//     }
+//     writeFileAsync('./db/db.json', JSON.stringify(notes), (err) =>
+//       err ? console.error(err) : console.log('note deleted! New notes: ', notes)).then(() => { res.json(notes) })
+//   })
+
 }
